d1d2d2d764e26bd18fdc948b2d5f5ea4589574be
Hassan-Ik <hassanikram08@gmail.com>
2024-10-27 18:18:50 +0000

    initialial portfolio commits learning and trying new things

---
+++ portfolio/data/index.ts
@@ -0,0 +1,8 @@
+export const gridItems = [
+    { id: 1, title: 'Title1', description: 'Desc1', className: '', img: '', imgClassName: '', titleClassName: '', spareImg: ''},
+    { id: 2, title: 'Title2', description: 'Desc2', className: '', img: '', imgClassName: '', titleClassName: '', spareImg: ''},
+    { id: 3, title: 'Title3', description: 'Desc3', className: '', img: '', imgClassName: '', titleClassName: '', spareImg: ''},
+    { id: 4, title: 'Title4', description: 'Desc4', className: '', img: '', imgClassName: '', titleClassName: '', spareImg: ''},
+    { id: 5, title: 'Title4', description: 'Desc4', className: '', img: '', imgClassName: '', titleClassName: '', spareImg: ''},
+    { id: 6, title: 'Title4', description: 'Desc4', className: '', img: '', imgClassName: '', titleClassName: '', spareImg: ''}
+]
